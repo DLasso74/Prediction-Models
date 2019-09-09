3b413fa2cbbf2ec6f0f5d7836862a7e04681e0e7
DLasso74 <36511318+DLasso74@users.noreply.github.com>
2019-09-09 16:34:07 -0500

    Add files via upload

--- House Price Prediction Linear Regression and GBR/house_price_predictionGBR.py
+++ House Price Prediction Linear Regression and GBR/house_price_predictionGBR.py
@@ -14,12 +14,25 @@ from sklearn import ensemble
 
 data = pd.read_csv('kc_house_data.csv')
 labels = data['price']
-conv_dates = [1 if values == 2010 else 0 for values in data.date]
-data['date'] = conv_dates
-train1 = data.drop(['id','price'],axis=1)
-x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size = 0.40, random_state = 2)
+data['yearsold'] = pd.DatetimeIndex(data['date']).year
+conv_dates = [1 if values == 2015 else 0 for values in data.yearsold]
+data['yearsold'] = conv_dates
+data['new'] = conv_dates
+for i in range(21613):
+    built = int(data.loc[i,['yr_built']])
+    remodeled = int(data.loc[i,['yr_renovated']])
+    if built > remodeled :
+        check = built
+    else:
+        check = remodeled
+    if check >= 2011:
+        data.iloc[i,22] = 1
+    else:
+        data.iloc[i,22] = 0
+train1 = data.drop(['id','price','date'],axis=1)
+x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size = 0.20, random_state = 2)
 
-model = ensemble.GradientBoostingRegressor(n_estimators = 400, max_depth = 5, min_samples_split = 2, learning_rate = 0.1, loss = 'ls')
+model = ensemble.GradientBoostingRegressor(n_estimators = 375, max_depth = 5, min_samples_split = 3, learning_rate = 0.09, loss = 'ls')
 model.fit(x_train,y_train)
-score = model.score(x_test,y_test)
-print("Model Accuracy = ",score)
+score = model.score(x_test,y_test)*100
+print("Model Accuracy =",score,"%")
--- House Price Prediction Linear Regression and GBR/house_price_predictionLR.py
+++ House Price Prediction Linear Regression and GBR/house_price_predictionLR.py
@@ -14,12 +14,13 @@ from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('kc_house_data.csv')
 labels = data['price']
-conv_dates = [1 if values == 2010 else 0 for values in data.date]
-data['date'] = conv_dates
-train1 = data.drop(['id','price'],axis=1)
-x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size = 0.40, random_state = 2)
+data['year'] = pd.DatetimeIndex(data['date']).year
+conv_dates = [1 if values == 2015 else 0 for values in data.year]
+data['year'] = conv_dates
+train1 = data.drop(['id','price','date'],axis=1)
+x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size = 0.20, random_state = 2)
 
 model = LinearRegression()
 model.fit(x_train,y_train)
 score = model.score(x_test,y_test)
-print("Model Accuracy = ",score)
+print("Model Accuracy =",score,"%")
