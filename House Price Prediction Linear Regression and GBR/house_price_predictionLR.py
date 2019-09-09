d3f44ffb1b17ffbfb782bb07fe77f69a3cffd0f3
DLasso74 <36511318+DLasso74@users.noreply.github.com>
2019-09-09 11:22:19 -0500

    Add files via upload

--- House Price Prediction Linear Regression and GBR/house_price_predictionLR.py
+++ House Price Prediction Linear Regression and GBR/house_price_predictionLR.py
@@ -9,18 +9,17 @@ Following the guidance of: https://towardsdatascience.com/create-a-model-to-pred
 """
 
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('kc_house_data.csv')
-
-model = LinearRegression()
-
 labels = data['price']
 conv_dates = [1 if values == 2010 else 0 for values in data.date]
 data['date'] = conv_dates
 train1 = data.drop(['id','price'],axis=1)
 x_train, x_test, y_train, y_test = train_test_split(train1, labels, test_size = 0.40, random_state = 2)
+
+model = LinearRegression()
 model.fit(x_train,y_train)
 score = model.score(x_test,y_test)
 print("Model Accuracy = ",score)
